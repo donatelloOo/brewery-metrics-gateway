d0138d1763f6d9ac901ce277c795859dad4cd29f
Donatello <donatien.riviere@gmail.com>
2026-05-25 03:45:08 +0200

    native compatibility with python 3.8, 3.9, 3.14

--- init.sh
+++ init.sh
@@ -3,12 +3,14 @@
 echo Initializing Brewery Metrics Gateway prerequisites...
 
 LOCAL_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+PYTHON_BIN="python3"
 
-# enable python3 virtual environment
-python3 -m venv "$LOCAL_PATH"/.venv
+# create/reset python3 virtual environment
+rm -fr $LOCAL_PATH/.venv
+$PYTHON_BIN -m venv "$LOCAL_PATH"/.venv
 source "$LOCAL_PATH"/.venv/bin/activate
 
 # add requirements
 python3 -m ensurepip --upgrade
-pip3 install --upgrade pip # setuptools wheel cython
+pip3 install --upgrade pip
 pip3 install -r requirements.txt
